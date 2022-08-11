9cd24ef4cd50413628625032f0980fd0126ffba3
dpoarch <davidpoarch@outlook.com>
2022-08-12 02:13:01 +0800

    Reconstruct Schema & Comments

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const cors = require("cors");
 const router = require("./src/routes/router.js");
 const mongoose = require('mongoose');
-const path = require('path');
 dotenv.config();
 
 // Server Port
@@ -22,12 +21,6 @@ mongoose.connect(process.env.MONGO_DB || 'mongodb://localhost:27017/athlete' , f
 app.use(express.json());
 app.use(cors());
 app.use("/api", router);
-
-
-// app.get("*", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
-// });
-
 
 app.listen(APP_PORT, () => console.log("Server running on port " + APP_PORT));
 
--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -1,53 +1,18 @@
 const express = require("express");
-const { validationResult, check } = require('express-validator');
+const { validationResult } = require('express-validator');
 const route = express.Router();
 const Athlete = require('../models/Athlete.js');
+const validate_create_profile_schema = require('../schema/validateCreateProfile');
+const validate_update_profile_schema = require('../schema/validateUpdateProfile');
 
-var postRequestVal = [
-  check('name')
-      .isLength({ max: 128 })
-      .exists({ checkNull: true, checkFalsy: true })
-      .withMessage('Must contain a parameter of "Name"'),
-  check('birthdate')
-      .exists({ checkNull: true, checkFalsy: true })
-      .withMessage('Must contain a parameter of "birthdate"'),
-  check('location')
-       .isLength({ max: 500 })
-      .exists({ checkNull: true, checkFalsy: true })
-      .withMessage('Must contain a parameter of "Location"'),
-  check('gender')
-      .isLength({ max: 6 })
-      .exists({ checkNull: true })
-      .withMessage('Must contain a parameter of "Gender"'),
-  check('sports')
-      .exists({ checkNull: true, checkFalsy: true })
-      .withMessage('Must contain a parameter of "Sports"'),
-  check('about')
-       .isLength({ max: 2000 })
-      .exists({ checkNull: true, checkFalsy: true })
-      .withMessage('Must contain a parameter of "about"'),
-  check('interests')
-      .isLength({ max: 255 })
-      .exists({ checkNull: true, checkFalsy: true })
-      .withMessage('Must contain a parameter of "interests"'),             
-];
-
-
-var putRequestVal = [
-  ...postRequestVal, 
-    check('_id')
-    .exists({ checkNull: true, checkFalsy: true })
-    .withMessage('Must contain a parameter of "_id"')
-]
-
+/* Get Athletes Profile */
 route.get('/athletes', async (req, res) => {
   var _atheletes = await Athlete.find({});
   return res.json(_atheletes);
 });
 
 /* Create Athelete Profile */
-route.post('/createProfile', postRequestVal, async (req, res) => {
-  /* [POST] Body Request
+/* [POST] Body Request
     "name": "",
     "birthdate": "",
     "location": "",
@@ -58,13 +23,15 @@ route.post('/createProfile', postRequestVal, async (req, res) => {
     "interests": "",
     "profileImg": ""
   */
-
+route.post('/createProfile', validate_create_profile_schema, async (req, res) => {
+  
   var checkValidate = validationResult(req);
 
   if (!checkValidate.isEmpty()) {
       var err = checkValidate.array().map(res => res.msg);
       return res.status(400).json({ message: err });
   }
+
   await Athlete.create(req.body);
 
   var _atheleteProfiles = await Athlete.find({});
@@ -72,8 +39,7 @@ route.post('/createProfile', postRequestVal, async (req, res) => {
 });
 
 /* Update Athelete Profile */
-route.put('/athletesProfile', putRequestVal, async (req, res) => {
-  /* [PUT] Body Request
+ /* [PUT] Body Request
     "name": "",
     "birthdate": "",
     "location": "",
@@ -84,7 +50,8 @@ route.put('/athletesProfile', putRequestVal, async (req, res) => {
     "interests": "",
     "profileImg": ""
   */
-
+route.put('/athletesProfile', validate_update_profile_schema, async (req, res) => {
+ 
     var checkValidate = validationResult(req);
 
     if (!checkValidate.isEmpty()) {
