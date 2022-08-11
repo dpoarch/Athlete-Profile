446e56b492151153c19d28e4cd71796924f1ac4a
dpoarch <davidpoarch@outlook.com>
2022-08-12 04:27:47 +0800

    update proxy configuration

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,14 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
      
 module.exports = function(app) {
-    app.use(createProxyMiddleware('/api', { target: 'https://serene-everglades-41183.herokuapp.com' }));
+    app.use(createProxyMiddleware('/api', { 
+        target: 'https://serene-everglades-41183.herokuapp.com',
+        "secure": false,
+        "changeOrigin": true,
+        headers: {
+            "Connection": "keep-alive"
+        },
+            }
+        )
+    );
 };
